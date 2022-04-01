67f57371780d68b1d2ae355352e708bd517314d3
Igor Dallazen Follador <igor_follador@sicredi.com.br>
2022-04-01 13:54:51 -0300

    Intial Structure

---
+++ src/main/java/uol/compass/shopStyle/checkout/MsCheckoutApplication.java
@@ -0,0 +1,13 @@
+package uol.compass.shopStyle.checkout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCheckoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCheckoutApplication.class, args);
+	}
+
+}
